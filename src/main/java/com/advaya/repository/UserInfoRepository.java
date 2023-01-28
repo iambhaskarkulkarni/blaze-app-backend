2cc1cb7de1799b036182babe53656d8118f3071c
Bhaskar Kulkarni <b.kulkarni1992@gmail.com>
2023-01-28 15:59:08 +0530

    spring security initial commit

---
+++ src/main/java/com/advaya/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.advaya.repository;
+
+import com.advaya.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+
+}
